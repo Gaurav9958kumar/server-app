ddc52e8e8038dee71c7164cc50f4e04a9b2c7b44
Gaurav Kumar <log4gaurav@gmail.com>
2022-12-05 08:09:40 +0530

    add

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ async function getData(){
     let res=await fetch(url);
     res=await res.json();
     console.log(res);
+    display(res);
 }
 
-async function storeData(){
+async function storeData(e){
+    e.preventDefault();
     let form=document.getElementById("form");
     let name=form.name;
     let image=form.image;
@@ -25,11 +27,32 @@ async function storeData(){
         id:Math.random()
     }
 
-    let res=await fetch(url,{
+     res=await fetch(url,{
         method: "POST",
       body:JSON.stringify(obj),
       headers:{"Content-Type":"application/json",},
     })
-    // getData();
+    getData();
     form.reset();
+}
+
+function display(data){
+    let container=document.getElementById("container");
+    container.innerHTML=null;
+    data.forEach(function(ele){
+     let div=document.createElement("div");
+     let image=document.createElement("img");
+     image.src=ele.image;
+     let name=document.createElement("h3");
+     name.innerText=ele.name;
+     let brand=document.createElement("h3");
+     brand.innerText=ele.brand;
+     let price=document.createElement("p");
+     price.innerText=ele.price;
+     let updateBtn=document.createElement("button");
+     updateBtn.innerText="Update Price"
+     div.append(image,name,brand,price,updateBtn);
+     container.append(div);
+
+    })
 }
