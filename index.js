4aa96151515d7367f200f1acdccbb155464a3db8
Gaurav Kumar <log4gaurav@gmail.com>
2022-12-05 08:51:47 +0530

    add

--- index.js
+++ index.js
@@ -5,35 +5,73 @@ window.onload=()=>{
 }
 
 async function getData(){
-    let res=await fetch(url);
+    res=await fetch(url)
     res=await res.json();
     console.log(res);
-    // display(res);
-}
+    display(res)
+ }
 
-async function storeData(e){
+ async function addDetails(e){
     e.preventDefault();
-    let form=document.getElementById("form");
-    let name=form.name;
-    let image=form.image;
-    let brand=form.brand;
-    let price=form.price;
+  let form=document.getElementById("form");
+  let name=form.name;
+  let brand=form.brand;
+  let price=form.price;
+  let image=form.image;
+    
+   let obj={
+    name:name.value,
+    brand:brand.value,
+    image:image.value,
+    
+    id:Math.random(),
+    price:+price.value
+   }
 
-    let obj={
-        name:name.value,
-        image:image.value,
-        brand:brand.value,
-        price:+price.value,
-        id:Math.random(),
-        status:true
-    }
+   let res=await fetch(url,{
+    method:"POST",
+    body:JSON.stringify(obj), headers: {
+        "Content-Type": "application/json",
+      },
+   })
+   
+   form.reset();
+   getData()
+ }
 
-     res=await fetch(url,{
-        method: "POST",
-      body:JSON.stringify(obj),
-      headers:{"Content-Type":"application/json",},
+
+     function display(data){
+     let container=document.getElementById("container");
+     container.innerHTML=null;
+     data.forEach(function(ele){
+      let div=document.createElement("div");
+      let image=document.createElement("img");
+      image.src=ele.image;
+      let name=document.createElement("h3");
+      name.innerText=ele.name;
+      let brand=document.createElement("h3");
+      brand.innerText=ele.brand;
+      let price=document.createElement("p");
+      price.innerText=ele.price;
+      let updateBtn=document.createElement("button");
+      updateBtn.innerText="Update Price"
+      updateBtn.onclick=()=>{
+        updatePrice();
+      }
+      div.append(image,name,brand,price,updateBtn);
+      container.append(div);
+
+     })
+ }
+
+
+ async function updatePrice(id){
+    let newPrice=window.prompt("enter new value");
+    let data={price:+newPrice}
+     fetch(`${url}/${id}`,{
+        method:"PATCH",
+        body:JSON.stringify(data),
+        headers:{"Content-Type": "application/json",},
     })
-     getData();
-    form.reset();
+    getData();
 }
-
